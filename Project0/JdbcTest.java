7fd1627aa710c504189e625296201d75fbaf3601
Sam KC <contact@kcsam.net>
2021-10-15 18:06:10 -0400

    changed Class.forName(oracle.jdbc.OracleDriver); to Class.forName(org.postgresql.Driver);

--- Project0/JdbcTest.java
+++ Project0/JdbcTest.java
@@ -1,22 +1,23 @@
-package testdb;
+
 
 import java.sql.*;    
 public class JdbcTest {
 	
-  private final static String DB_URL = "jdbc:postgresql://localhost:5432/";
+  private final static String DB_URL = "jdbc:postgresql://localhost:5432/postgres";
   private final static String USER = "postgres";
   private final static String PASS = "admin";
 
   public static void main(String[] args) {
     Connection conn = null;  
     try {    
-      Class.forName("oracle.jdbc.OracleDriver");    
+    	Class.forName("org.postgresql.Driver");
       System.out.println("Connecting to database...");    
       conn = DriverManager.getConnection(DB_URL,USER,PASS);    
     } catch (Exception e) {    
       e.printStackTrace();    
     } finally {    
-      if (conn != null) {    
+      if (conn != null) {  
+    	  System.out.println("Connected");    
         try {    
           conn.close();    
         } catch (SQLException e) {    
