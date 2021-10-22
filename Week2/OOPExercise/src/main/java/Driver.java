fce7c4f832c1a3227235c0cb6bf6809cbe469c23
SamKC0 <92332915+SamKC0@users.noreply.github.com>
2021-10-22 18:14:48 -0400

    Update Driver.java
    Added additional conditional statements on main method.

--- Week2/OOPExercise/src/main/java/Driver.java
+++ Week2/OOPExercise/src/main/java/Driver.java
@@ -7,25 +7,32 @@ import com.revature.models.Plant;
 
 public class Driver {
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) {		
 		
 		double height = getHeight();
+		
+		if (height != 0) {
 		Algae redAlgae = new Algae("red algae", "red", height, "Unknown", false);
 		System.out.println(redAlgae);
+		}
 		
 	}
 	
 	public static double getHeight() {
 		System.out.println("Enter height for plant");
 		Scanner sc = new Scanner(System.in);
+		double height = 0;
 		try {
 			double choice = sc.nextDouble();
+			height = choice;
+			sc.close();
 			}
 			catch(Exception e) {
+			sc.close();
 			 System.out.println("error, invalid data type");
 			}
 		
-		return 0;		
+		return height;		
 	}
 	
 }
